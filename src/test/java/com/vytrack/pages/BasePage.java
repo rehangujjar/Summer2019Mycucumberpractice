8fbc95f823d4a243355dea0e0d40292163281e1b
RehanGujjar <rehanmehmood507@gmail.com>
2020-03-04 07:05:46 -0500

    third

--- src/test/java/com/vytrack/pages/BasePage.java
+++ src/test/java/com/vytrack/pages/BasePage.java
@@ -13,7 +13,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 //everything that is in common among pages
-
+//can go here
 //for example top menu elements don't belong to specific page
 //top menu appears on every single page
 //so we can keep them here
